bce1f41f44dff9a89bc05a8c83502beafc4c24b2
Rishi Shah <rishihahs@gmail.com>
2015-04-19 13:40:15 -0500

    Symlinks fixed

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 CURRENT_DIR="${0:a:h}"
 setopt EXTENDED_GLOB
 
-# Symlink all dotfiles in dotfiles directory
-for dotfile in "${CURRENT_DIR}"/dotfiles/.*; do
-  ln -s "$dotfile" "${ZDOTDIR:-$HOME}/.${dotfile:t}"
-done
-
 # Symlink prezto files to home directory
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# Symlink all dotfiles in dotfiles directory
+for dotfile in "${CURRENT_DIR}"/dotfiles/.*; do
+  ln -sfn "$dotfile" "${ZDOTDIR:-$HOME}/${dotfile:t}"
 done
