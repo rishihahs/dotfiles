1ebe34504e2edad7a53f374656a52048abb58302
Rishi Shah <rishihahs@gmail.com>
2016-05-22 20:08:04 -0500

    Download TPM and install neovim config

--- install.sh
+++ install.sh
@@ -9,11 +9,20 @@ if [[ -d "${CURRENT_DIR}/dotfiles/.zprezto" ]]; then
 fi
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${CURRENT_DIR}/dotfiles/.zprezto"
 
-# Download Vim Vundle
-if [[ -d "${CURRENT_DIR}/dotfiles/.vim/bundle/Vundle.vim" ]]; then
-    rm -rf "${CURRENT_DIR}/dotfiles/.vim/bundle/Vundle.vim"
+# Download tmux plugin manager
+if [[ -d "${CURRENT_DIR}/dotfiles/.tmux" ]]; then
+    rm -rf "${CURRENT_DIR}/dotfiles/.tmux"
 fi
-git clone https://github.com/gmarik/Vundle.vim.git "${CURRENT_DIR}/dotfiles/.vim/bundle/Vundle.vim"
+git clone https://github.com/tmux-plugins/tpm "${CURRENT_DIR}/dotfiles/.tmux/plugins/tpm"
+
+# Create nvim directory
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+if [[ -d "${XDG_CONFIG_HOME}/nvim" ]]; then
+    mv "${XDG_CONFIG_HOME}/nvim" "${XDG_CONFIG_HOME}/nvim.bak"
+fi
+
+# Download Vim Dein
+sh <(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh) ${XDG_CONFIG_HOME}/nvim
 
 # Symlink prezto files to home directory
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
@@ -24,3 +33,6 @@ done
 for dotfile in "${CURRENT_DIR}"/dotfiles/.*; do
   ln -sfn "$dotfile" "${ZDOTDIR:-$HOME}/${dotfile:t}"
 done
+
+# Symlink .vimrc to .config/nvim/init.vim
+ln -s "${CURRENT_DIR}/dotfiles/.vimrc" $XDG_CONFIG_HOME/nvim/init.vim
